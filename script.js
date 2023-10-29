1935a5ecc80b16480dd3fbebebceb83f1ee0beb9
MAG-ENCRYPTION <mbogerard03@gmail.com>
2023-10-30 00:22:46 +0100

    Presentation front end

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-const btns = document.querySelectorAll('.btn');
-const resultat = document.getElementById('resultat');
-const clear = document.getElementById('clear');
-const equal = document.getElementById('equal');
+// Sélectionner le bouton du formulaire
+const button = document.querySelector('button');
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        resultat.value = resultat.value + btn.textContent;
-    });
-});
+// Sélectionner l'élément où l'image sera affichée
+const qrCodeSection = document.querySelector('section div');
 
-clear.addEventListener('click', () => {
-    resultat.value = "";
-});
+// Fonction pour gérer le clic sur le bouton
+function handleClick() {
+  // Créer l'élément de l'image
+  const image = document.createElement('img');
+  image.src = 'example.jpg';
+  image.alt = 'QR Code';
 
-equal.addEventListener('click', () => {
-    resultat.value = eval(resultat.value);
-})
+  // Ajouter la classe pour l'animation slideInRight
+  image.classList.add('slide-in-right');
+  image.animate([{
+    transform: 'translateX(100%)',
+},
+ {
+    transform: 'translateX(0)',
+}],{// options de l'animation
+duration: 1000,
+easing: 'ease-in-out', 
+fill: 'forwards'})
+
+  // Ajouter l'image à la section du QR code
+  qrCodeSection.appendChild(image);
+}
+
+// Ajouter l'événement clic au bouton
+button.addEventListener('click', handleClick);
